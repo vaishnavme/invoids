de7a03b02ea20f79784fb2164e216b07f4fd8388
Vaishnav Chandurkar <42497931+vaishnavme@users.noreply.github.com>
2024-02-22 20:54:07 +0530

    feat: basic app layout, dark mode (#1)

---
+++ components/Layout/SideNavbar.tsx
@@ -0,0 +1,64 @@
+import { Button } from "../UI/Button";
+import ToggleTheme from "./ToggleTheme";
+import Icon from "../UI/Icons";
+
+const navbarItems = [
+  {
+    label: "Create New",
+    icon: <Icon.PencilPlus strokeWidth={1.5} size={20} />,
+  },
+  {
+    label: "Folder",
+    icon: <Icon.Folder strokeWidth={1.5} size={20} />,
+  },
+  {
+    label: "Search",
+    icon: <Icon.Search strokeWidth={1.5} size={20} />,
+  },
+  {
+    label: "Bookmarks",
+    icon: <Icon.Bookmarks strokeWidth={1.5} size={20} />,
+  },
+];
+
+const SideNavbar = () => {
+  return (
+    <div className="h-screen w-11 bg-neutral-100 dark:bg-neutral-800 px-1 py-1.5 flex flex-col items-center justify-between border-r border-neutral-200 dark:border-neutral-700">
+      <div className="flex flex-col items-center">
+        <Button variant="ghost" size="icon" className="mb-6">
+          <Icon.LeftSidebar strokeWidth={1.5} size={20} />
+        </Button>
+
+        <div className="flex flex-col items-center">
+          {navbarItems.map((item) => (
+            <Button
+              key={item.label}
+              variant="ghost"
+              size="icon"
+              aria-label={item.label}
+              className="mb-4"
+              title={item.label}
+            >
+              {item.icon}
+            </Button>
+          ))}
+        </div>
+      </div>
+
+      <div className="flex flex-col items-center">
+        <ToggleTheme />
+        <Button
+          variant="ghost"
+          size="icon"
+          aria-label="Settings"
+          title="Settings"
+          className="mb-2"
+        >
+          <Icon.Settings strokeWidth={1.5} size={20} />
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default SideNavbar;
---
+++ components/ui/Icons.tsx
@@ -0,0 +1,23 @@
+import {
+  IconBookmarks,
+  IconFolder,
+  IconLayoutSidebarLeftExpand,
+  IconMoonStars,
+  IconPencilPlus,
+  IconSearch,
+  IconSettings,
+  IconSun,
+} from "@tabler/icons-react";
+
+const Icon = {
+  PencilPlus: IconPencilPlus,
+  Folder: IconFolder,
+  Search: IconSearch,
+  Bookmarks: IconBookmarks,
+  LeftSidebar: IconLayoutSidebarLeftExpand,
+  Settings: IconSettings,
+  Sun: IconSun,
+  MoonStar: IconMoonStars,
+};
+
+export default Icon;
