ac828bcf92024a83108d4cd4159d737de8de1f22
Vaishnav Chandurkar <42497931+vaishnavme@users.noreply.github.com>
2024-03-10 23:42:49 +0530

    feat: read markdown file content, convert it to html and initialise it (#11)

--- components/Editor/Editor.tsx
+++ components/Editor/Editor.tsx
@@ -1,18 +1,27 @@
+import {
+  forwardRef,
+  useEffect,
+  useImperativeHandle,
+  ForwardedRef,
+} from "react";
 import { useEditor, EditorContent } from "@tiptap/react";
 import editorExtensions from "./Extension";
 import EditorHoverMenu from "./Extension/EditorHoverMenu";
 
 type IEditorProps = {
-  onUpdate: (data: any) => void;
+  content: string;
+  // eslint-disable-next-line no-unused-vars
+  onUpdate: (data: string) => void;
 };
 
-const Editor = (props: IEditorProps) => {
-  const { onUpdate } = props;
+const Editor = forwardRef((props: IEditorProps, ref: ForwardedRef<any>) => {
+  const { onUpdate, content } = props;
 
   const editor = useEditor({
     extensions: editorExtensions,
-    onUpdate: ({ editor }) => {
-      const html = editor.getHTML();
+    content,
+    onUpdate: ({ editor: editorInstance }) => {
+      const html = editorInstance.getHTML();
       onUpdate(html);
     },
     editorProps: {
@@ -22,12 +31,30 @@ const Editor = (props: IEditorProps) => {
     },
   });
 
+  useImperativeHandle(
+    ref,
+    () => ({
+      clearContent: () => {
+        if (editor) {
+          editor.commands.clearContent();
+        }
+      },
+    }),
+    [editor],
+  );
+
+  useEffect(() => {
+    if (editor && editor.isEmpty && content !== "") {
+      editor.commands.insertContent(content);
+    }
+  }, [editor, content]);
+
   return (
     <>
       <EditorHoverMenu editor={editor} />
       <EditorContent editor={editor} />
     </>
   );
-};
+});
 
 export default Editor;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,53 @@
+import { useEffect, useRef, useState } from "react";
+import { useSelector } from "react-redux";
+import { useRouter } from "next/router";
 import Editor from "@/components/Editor/Editor";
 import { Textarea } from "@/components/UI/Textarea";
 import useAutosizeTextArea from "@/hooks/useAutosizeTextArea";
-import { useRef, useState } from "react";
+import contentService from "@/lib/contentServices";
+import { toast } from "sonner";
 
 const Home = () => {
+  const router = useRouter();
+
+  const { appConfig } = useSelector((state) => state.AppData);
+
   const docTitleRef = useRef(null);
+  const docsBodyRef = useRef(null);
 
   const [docTitle, setDocTitle] = useState<string>("Untitled");
-  const [docsBody, setDocsBody] = useState<string>("");
+  const [docsBody, setDocBody] = useState<string>("");
 
   useAutosizeTextArea(docTitleRef.current, docTitle);
+
+  const loadExistingNote = async (fileName: string) => {
+    const fs = await import("@tauri-apps/api/fs");
+
+    const filePath = `${appConfig.path}/${fileName}.md`;
+
+    try {
+      const response = await fs.readTextFile(filePath);
+      const { frontMatter, body } = await contentService.getHTMLText(response);
+
+      setDocTitle(frontMatter.title);
+      setDocBody(body);
+    } catch (err) {
+      toast.error("Could not load note. Please try again later.");
+    }
+  };
+
+  const handleDocBodyUpdate = (textContent: string) => setDocBody(textContent);
+
+  useEffect(() => {
+    const slug = (router?.query?.slug as string) || "";
+    if (slug) {
+      loadExistingNote(slug);
+    } else {
+      setDocTitle("Untitled");
+      setDocBody("");
+      docsBodyRef?.current?.clearContent();
+    }
+  }, [router]);
 
   return (
     <div className="max-w-3xl w-full mx-auto p-6">
@@ -19,14 +57,18 @@ const Home = () => {
       <Textarea
         ref={docTitleRef}
         autoFocus
-        defaultValue={docTitle}
+        value={docTitle}
         placeholder="Untitled"
         maxLength={180}
         onChange={(e) => setDocTitle(e.target.value)}
         className="text-3xl font-semibold w-full mb-4 h-0 outline-none border-none dark:border-none resize-none focus-visible:ring-transparent dark:focus-visible:ring-transparent shadow-none"
       />
       <div className="mb-64 px-4">
-        <Editor onUpdate={setDocsBody} />
+        <Editor
+          ref={docsBodyRef}
+          content={docsBody}
+          onUpdate={(data) => handleDocBodyUpdate(data)}
+        />
       </div>
     </div>
   );
