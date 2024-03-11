8f97343993fb813e699bc538fb1332a87d1d19e0
Vaishnav <vaishnavchandurkar12@gmail.com>
2024-03-11 21:55:48 +0530

    add: code block hightlight support

--- components/Editor/Extension/index.ts
+++ components/Editor/Extension/index.ts
@@ -3,14 +3,24 @@ import Placeholder from "@tiptap/extension-placeholder";
 import TaskItem from "@tiptap/extension-task-item";
 import TaskList from "@tiptap/extension-task-list";
 import Underline from "@tiptap/extension-underline";
+import CodeBlockLowlight from "@tiptap/extension-code-block-lowlight";
+import { common, createLowlight } from "lowlight";
 import SlashCommand from "./SlashCommand";
 
 const editorExtensions = [
-  StarterKit,
+  StarterKit.configure({
+    codeBlock: false,
+  }),
   SlashCommand,
   TaskItem,
   TaskList,
   Underline,
+  CodeBlockLowlight.configure({
+    lowlight: createLowlight(common),
+    HTMLAttributes: {
+      class: "hljs",
+    },
+  }),
   Placeholder.configure({
     placeholder: "Sketch, Write, Organize",
   }),
