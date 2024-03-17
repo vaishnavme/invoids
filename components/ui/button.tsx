08e14f47b54303ef54fbeb76643be4ced5e86409
Vaishnav <vaishnavchandurkar12@gmail.com>
2024-03-17 14:22:07 +0530

    add: Docs overview info

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -5,7 +5,7 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-lg text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-neutral-950 disabled:pointer-events-none disabled:opacity-50 dark:focus-visible:ring-neutral-300",
+  "inline-flex items-center justify-center whitespace-nowrap rounded-lg text-sm font-medium transition-colors outline-none focus:ring-2 disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
@@ -32,12 +32,13 @@ const buttonVariants = cva(
       variant: "default",
       size: "default",
     },
-  }
+  },
 );
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
+  // eslint-disable-next-line react/require-default-props
   asChild?: boolean;
 }
 
@@ -51,7 +52,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
         {...props}
       />
     );
-  }
+  },
 );
 Button.displayName = "Button";
 
--- lib/contentServices.ts
+++ lib/contentServices.ts
@@ -1,13 +1,10 @@
 import Showdown from "showdown";
 import matter from "gray-matter";
-
-type FrontMatter = {
-  title: string;
-  slug: string;
-  createdAt: number;
-  updatedAt: number;
-  [key: string]: string | number;
-};
+import { HEADINGS } from "./constant";
+import {
+  FrontMatter,
+  TableOfContentElement,
+} from "./types/content.service.types";
 
 type GetMarkDownString = {
   body: string;
@@ -39,9 +36,42 @@ const getMarkdownString = async ({ body, metaData }: GetMarkDownString) => {
   return markdownString;
 };
 
+const getOverviewInfo = (textContent: string) => {
+  const words = textContent.trim().split(/\s+/).length;
+  const characters = textContent?.replaceAll(" ", "")?.length;
+
+  const readTime = words > 0 ? Math.ceil(words / 200) : 0;
+
+  return {
+    words,
+    characters,
+    readTime,
+  };
+};
+
+const getTableOfContentFromDOM = (domNode: HTMLElement) => {
+  const rawHeadingTags = domNode?.querySelectorAll("h1, h2, h3, h4, h5, h6");
+
+  const tableOfContent: TableOfContentElement[] = [];
+
+  rawHeadingTags?.forEach((tag) => {
+    const tagName = tag?.tagName;
+    const tagInfo = HEADINGS[tagName as keyof typeof HEADINGS];
+    const element: TableOfContentElement = {
+      text: tag?.textContent || null,
+      ...tagInfo,
+    };
+    tableOfContent.push(element);
+  });
+
+  return tableOfContent;
+};
+
 const contentService = {
   getHTMLText,
   getMarkdownString,
+  getOverviewInfo,
+  getTableOfContentFromDOM,
 };
 
 export default contentService;
